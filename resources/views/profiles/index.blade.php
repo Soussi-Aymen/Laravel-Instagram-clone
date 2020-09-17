b3fe432f3c9fc0e8223bf67cbe9d7a3effb4da8a
Aymen <aymensoussi.02@gmail.com>
2020-09-17 13:15:33 +0100

    making progress .

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,14 +4,20 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://instagram.ftun12-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.ftun12-1.fna.fbcdn.net&_nc_ohc=Bx-TAUyNgPgAX-t5vCn&oh=72ed3c4c2c9c0c8136fecaf340a1ddf1&oe=5F861667" class="rounded-circle" />
+            <img src="/storage/{{$user->profile->image}}" class="rounded-circle w-100" />
         </div>
 
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1> {{$user->username}}</h1>
-                <a href='/p/create'> Add New Post </a>
+                @can('update',$user->profile)
+                    <a href='/p/create'> Add New Post </a>
+                @endcan
+
             </div>
+            @can('update',$user->profile)
+                <a href='/profile/{{$user->id}}/edit'> Edit Profile </a>
+            @endcan
             <div class="d-flex">
                 <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts </div>
                 <div class="pr-5"><strong>55.9k</strong> followers</div>
