e4e2ae63fffc28c88cb175470d79be5e6617ffdb
Aymen <aymensoussi.02@gmail.com>
2020-09-18 17:49:00 +0100

    cache added

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 
@@ -14,7 +15,25 @@ class ProfilesController extends Controller
     {
         $follows = (auth()->user() ) ? auth()->user()->following->contains($user->id) : false ;         
         
-        return view('profiles.index',compact('user','follows'));
+        $postCount = Cache::remember('count.posts' .$user->id ,
+        now()->addSeconds(30) ,
+         function() use ($user){
+            return $user->posts->count();
+        }) ;
+        $followersCount = Cache::remember('count.followers' .$user->id ,
+        now()->addSeconds(30) ,
+         function() use ($user){
+            return $user->profile->followers->count();
+        }) ;
+         
+        $followingCount =Cache::remember('count.following' .$user->id ,
+        now()->addSeconds(30) ,
+         function() use ($user){
+            return  $user->following->count();
+
+        }) ;
+
+        return view('profiles.index',compact('user','follows','postCount' ,'followersCount','followingCount' ));
 
     }
 
